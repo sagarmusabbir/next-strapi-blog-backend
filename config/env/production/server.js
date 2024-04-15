3f3d7a37f797700e849f5166ff5e525e2d01471d
sagarmusabbir <sagarmusabbir@gmail.com>
2024-04-16 04:43:45 +0600

    b2b

--- config/env/production/server.js
+++
@@ -1,7 +0,0 @@
-module.exports = ({ env }) => ({
-  proxy: true,
-  url: env("APP_URL"), // Sets the public URL of the application.
-  app: {
-    keys: env.array("APP_KEYS", "d5d7cdbc107510045117eb4bb1c0b6ce"),
-  },
-});
